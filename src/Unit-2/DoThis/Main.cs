2ac364db15d93c6c1cde477ad641348c915ad00b
jkone27 <jkone27@yahoo.it>
2016-09-04 13:09:54 +0200

    Unit2 Lesson1 runtime error - mono

--- src/Unit-2/DoThis/Main.cs
+++ src/Unit-2/DoThis/Main.cs
@@ -23,12 +23,16 @@ namespace ChartApp
 
         private void Main_Load(object sender, EventArgs e)
         {
-            _chartActor = Program.ChartActors.ActorOf(Props.Create(() => new ChartingActor(sysChart)), "charting");
-            var series = ChartDataHelper.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
-            _chartActor.Tell(new ChartingActor.InitializeChart(new Dictionary<string, Series>()
-            {
-                {series.Name, series}
-            }));
+            _chartActor = Program.ChartActors
+				.ActorOf(Props.Create(() => new ChartingActor(sysChart)), "charting");
+            var series = ChartDataHelper
+				.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
+            _chartActor
+				.Tell(new ChartingActor.InitializeChart(
+					new Dictionary<string, Series>()
+			            {
+			                {series.Name, series}
+			            }));
         }
 
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
